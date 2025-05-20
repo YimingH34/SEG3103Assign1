b173f105184630c064ee38746c568a13c79f5c51
YimighH34 <ming8582@hotmail.com>
2025-05-20 17:37:23 -0400

    t

--- test1_firefox.py
+++ test1_firefox.py
@@ -21,7 +21,7 @@ time.sleep(3)
 continue_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Strictly Necessary']")))
 continue_btn.click()
 
-time.sleep(5)
+time.sleep(3)
 search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test = "search-input-button"]')))
 search.click()
 
@@ -29,3 +29,24 @@ txt_input = driver.find_element(By.ID,"desktop-search-search-input")
 
 txt_input.send_keys("yoshi")
 txt_input.send_keys(Keys.ENTER)
+
+time.sleep(3)
+element = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//a[@data-test="product-leaf-title" and contains(., "Super Mario World")]'))
+)
+element.click()
+
+time.sleep(3)
+search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test = "add-to-cart-skroll-cta"]')))
+search.click()
+
+time.sleep(3)
+search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test = "view-my-bag"]')))
+search.click()
+
+
+# check if qty = 1
+
+
+# check if item exist
+# check if price is 169.99
